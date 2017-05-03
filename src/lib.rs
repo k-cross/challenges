3662871f301bd04d9a1e80e640c1f8ea08a9ca0f
ken <kenneth.cross@sjsu.edu>
2017-05-03 07:44:35 -0700

    green test and new red test, refactor later

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,55 @@
+//! This module takes a string and returns a specified number of words where the order
+//! of words returned occur from most frequent to least frequent. Tie breakers are
+//! ordered arbitrarily but most likely alphabetically according to language.
+
 extern crate regex;
+//extern crate ordermap;
 use regex::Regex;
-
-
-/// This function is made to vectorize an english string into a list
-// TODO: Change approach, needs to run in O(n) and this might be too expensive.
-pub fn word_list(text: &str) -> Vec<&str> {
-    let re = Regex::new(r"").unwrap();
-    let mut list = vec![];
-
-    list
-}
-
-pub fn word_freq_list(roman_text: &str, num_of_items: u64) -> Vec<&str> {
-    assert!(roman_text == "roman alphabet" && num_of_items > 0); // TODO: fix
-    println!("init");
-
-    vec!["returned", "value"]
-}
+use std::collections::HashMap;
+//use ordermap::OrderMap;
 
 
 #[cfg(test)]
-mod tests {
-    //use super::*;
+mod unit_tests {
+    use super::*;
 
     #[test]
     fn test_string_vectorized() {
         let test_string = "(This) {is} <a> [test] okay! So, just relax...";
-        let test_list = super::word_list(test_string);
-        assert_eq!(vec!["This","is","a","test","okay","So","just","relax"], test_list);
+        let test_list = word_list(test_string);
+        assert_eq!(vec!["this","is","a","test","okay","so","just","relax"], test_list);
     }
+
+    #[test]
+    fn test_mapping_and_ordering() {
+        let test_string = "(This) {is} <a> [test] okay! So, just relax...";
+        let test_map = word_freq_list(test_string, 3);
+        assert_eq!(vec!["this","is","a"], test_map);
+    }
+}
+
+/// Word List is a helper function made to vectorize an english string into a vector
+fn word_list(text: &str) -> Vec<String> {
+    let re = Regex::new(r"[0-9a-zA-Z\-]+").unwrap();
+    let mut list: Vec<String> = vec![];
+
+    for item in re.captures_iter(&text) {
+        let word = item[0].to_string().to_lowercase();
+        list.push(word);
+    }
+
+    list
+}
+
+/// Word Frequency List is a function that takes two arguments, a string and the
+/// number of items to return. If the specified number of items to return is larger 
+/// than the number of items that can be returned, the largest possible number will be
+/// returned.
+// TODO: combine word_list and word_freq_list later to speed up computation
+pub fn word_freq_list(roman_text: &str, num_of_items: u64) -> Vec<&str> {
+    assert!(num_of_items > 0);
+    let list = word_list(roman_text);
+    let mut map: HashMap<String, u64> = HashMap::new();
+
+    vec!["returned", "value"]
 }
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,37 @@
 
 extern crate wc;
 
+// testing
+extern crate ordermap;
+use ordermap::OrderMap;
+use std::collections::HashMap;
+
 fn main() {
     println!("init");
     let rmnabt: &str = "roman alphabet";
+    let test_string = "(This) {is} <a> [test] okay! So, just relax...";
     let freq_list = wc::word_freq_list(&rmnabt, 2);
+    let mut om = OrderMap::new();
+    let mut hm = HashMap::new();
+
+    hm.insert("a", 1);
+    hm.insert("a", 1);
+    hm.insert("b", 1);
+    hm.insert("c", 1);
+    hm.insert("d", 3);
+    hm.insert("e", 1);
+
+ //   let mut mapVec: Vec<_> = vec![];
+
+    om.insert("a", 1);
+    om.insert("b", 1);
+    om.insert("c", 1);
+    om.insert("d", 3);
+    om.insert("e", 1);
+
+    for (k,v) in &hm {
+        println!("{} {}",k,v);
+    }
+
     println!("{}", &freq_list[0]);
 }
